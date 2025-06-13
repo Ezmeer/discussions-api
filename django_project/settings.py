f7ca1cbb3f405b9258e9d9c992d3b0c62b6cf783
Michael Yehoyakim <myehoyakim@plushcare.com>
2025-06-13 17:51:00 -0400

    add discussions app

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'discussions.apps.DiscussionsConfig',
 ]
 
 MIDDLEWARE = [
